aa5ce1f9c5e760bd721686029dc57536541191ae
Michelle Fischer <Michelle.Fischer@BK.local>
2022-08-05 14:56:13 +0200

    New stuff

--- Assets/MyProject/Scripts/PlayerController.cs
+++ Assets/MyProject/Scripts/PlayerController.cs
@@ -29,29 +29,36 @@ public class PlayerController : MonoBehaviour, IActor
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f || Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
                 direction = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
-
                 if (direction != Vector3.zero)
                 {
-
                     var moveCommand = new MoveCommand(this, direction, movePoint);
                     _commandProcessor.ExecuteCommand(moveCommand);
-
                 }
-
             }
         }
-        //IMPLEMENT: GETKEY(); COnstant hold down for back properly.
-            if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+            if (Input.GetKey(KeyCode.Backspace))
             {
-                Debug.Log("Backspace pressed");
                 _commandProcessor.Undo();
             }
+        }
+
         
     }
 
     public void MoveFromTo(Vector3 startPos, Vector3 endPos)
     {
         throw new System.NotImplementedException();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "_itemToCollect")
+        {
+            collision.gameObject.SetActive(false);
+        }
     }
 }
 
